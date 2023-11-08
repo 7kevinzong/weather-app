2253900094760735b56312a0e76a9c8a17d5f226
Kevin Zong <7kevinzong@gmail.com>
2023-11-07 23:30:32 -0500

    feat: add basic city search functionality

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '39b8394798msh5e396f75e69c897p16eb20jsne4d982956bce',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
