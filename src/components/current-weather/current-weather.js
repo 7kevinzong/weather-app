6a7d1ae5d1c8adac64b3ec7806a7ad9ff7c3fb8e
Kevin Zong <7kevinzong@gmail.com>
2023-11-08 00:04:23 -0500

    feat: display current temperature and additional weather details

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -10,6 +10,30 @@ const CurrentWeather = () => {
         </div>
         <img src="icons/01d.png" alt="weather" className="weather-icon" />
       </div>
+      <div className="bottom">
+        <p className="temperature">88°F</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">92°F</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">2 mph</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">15%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">15 hPa</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
